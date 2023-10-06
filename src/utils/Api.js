7a0f7372cf954d0125dbe5c89c22c3fb339a9bb4
uesleipedro <ielseu@hotmail.com>
2023-10-06 10:01:11 -0300

    alter baseurl axios

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'http://134.122.24.222:3333/',
-    // baseURL: 'http://localhost:3333/',
+    // baseURL: 'http://134.122.24.222:3333/',
+    baseURL: 'http://localhost:3333/',
     headers: {
         "Cache-Control": "no-cache",
         "Content-Type": "application/x-www-form-urlencoded",
