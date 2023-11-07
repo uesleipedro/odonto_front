bff54501bc118c7c18da7c45b6a7e7d906c9e169
uesleipedro <ielseu@hotmail.com>
2023-11-07 10:58:56 -0300

    change api

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,8 +3,8 @@ import 'dotenv/config'
 
 const api = axios.create({
     // baseURL: 'http://134.122.24.222:3333/',
-    baseURL: 'http://localhost:3333/',
-    // baseURL: `${process.env.NEXT_PUBLIC_PATH}`,
+    // baseURL: 'http://localhost:3333/',
+    baseURL: `${process.env.NEXT_PUBLIC_PATH}`,
     headers: {
         "Cache-Control": "no-cache",
         "Content-Type": "application/x-www-form-urlencoded",
